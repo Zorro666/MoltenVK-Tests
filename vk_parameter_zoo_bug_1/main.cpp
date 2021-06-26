38f818c075abe97e6ff0d72186416b26bcbb55e0
Jake Turner <jake@evansturner.co.uk>
2021-06-26 07:21:35 +0100

    Separated first bug into its own project

---
+++ vk_parameter_zoo_bug_1/main.cpp
@@ -0,0 +1,21 @@
+#include <iterator>
+
+#include "common/common.hpp"
+
+int main(int argc, const char * argv[])
+{
+  Context context;
+  initializeContext(context, "vk_parameter_zoo_bug_1");
+
+  /*
+   pTessellationState is ignored if the pipeline does not include a tessellation control shader stage and tessellation evaluation shader stage.
+   */
+  context.basePipelineCreateInfo.pTessellationState = (VkPipelineTessellationStateCreateInfo *)0x1234;
+  createPipeline(context);
+  destroyPipeline(context);
+
+  printf("Hello, World!\n");
+
+  destroyContext(context);
+  return 1;
+}
